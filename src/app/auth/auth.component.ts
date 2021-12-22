8778e54b608b633a1c55b2dc827b039936d6dad6
vytautas.krapikas <vytautas.krapikas@sofdent.lt>
2021-12-22 09:28:51 +0200

    Fix(auth) login validation working

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
+import { BehaviorSubject } from "rxjs";
 import { User } from "../shared/dto/user.model";
 import { AuthService } from "../shared/services/auth/auth.service";
 import { ApiService } from "../shared/services/questions/api.service";
+
+export const TokenHeaderName = "OnlineExamToken";
+export const SessionHeaderName = "OnlineExamSession";
 
 @Component({
   selector: "app-auth",
@@ -10,32 +14,63 @@ import { ApiService } from "../shared/services/questions/api.service";
   styleUrls: ["./auth.component.scss"],
 })
 export class AuthComponent implements OnInit {
+  private storage = window.localStorage;
+
   formInput: User = {
     userName: "",
     password: "",
   };
 
+  // private session: BehaviorSubject<User | null> = new BehaviorSubject<User | null>(this.SavedSession);
+
   constructor(private apiService: ApiService, private authService: AuthService, private route: ActivatedRoute, private router: Router) {}
+
+  // private get SavedSession(): User | null {
+  //   const savedSessionJSON = this.storage.getItem(SessionHeaderName as string);
+  //   if (savedSessionJSON && savedSessionJSON.length > 0) {
+  //     const savedSession = JSON.parse(savedSessionJSON) as User;
+  //     if (savedSession) {
+  //       return savedSession;
+  //     }
+  //   }
+  //   return null;
+  // }
 
   ngOnInit() {
     this.allUsers();
+    console.log("Storage", this.storage);
   }
+
+  // private set NewSession(session: User | null) {
+  //   this.session.next(session);
+  // }
+
+  // private set SaveSession(session: User) {
+  //   this.storage.setItem(SessionHeaderName as string, JSON.stringify(session));
+  // }
 
   tryLogin(form: any) {
     const userName = form.controls.userName.value ?? "";
     const password = form.controls.password.value ?? "";
 
-    this.authService
-      .Login(userName, password)
-      .pipe()
-      .subscribe(
-        () => {
-          this.router.navigateByUrl("/home");
-        },
-        (Error) => {
-          console.log(Error);
+    this.authService.Login().subscribe(
+      (res) => {
+        const user = res.find((x: any) => {
+          return x.userName === userName && x.password === password;
+        });
+        if (user) {
+          alert("Login success");
+          this.router.navigate(["/home"]);
+          // this.NewSession = res;
+          // this.SaveSession = res;
+        } else {
+          alert("user not found");
         }
-      );
+      },
+      (Error) => {
+        console.log(Error);
+      }
+    );
   }
 
   allUsers() {
@@ -47,4 +82,10 @@ export class AuthComponent implements OnInit {
   checkUser() {
     this.apiService.getUsers().subscribe((x) => {});
   }
+
+  // LogOut() {
+  //   this.NewSession = null;
+  //   this.storage.removeItem(TokenHeaderName);
+  //   this.storage.clear();
+  // }
 }
--- src/app/shared/services/auth/auth.service.ts
+++ src/app/shared/services/auth/auth.service.ts
@@ -1,27 +1,13 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
-import { map } from "rxjs/operators";
-import { User } from "../../dto/user.model";
 
 @Injectable({
   providedIn: "root",
 })
 export class AuthService {
-  private userSubject: BehaviorSubject<User>;
-
   constructor(private http: HttpClient) {}
 
-  Login(userName: string, password: string) {
-    // if(userName !== undefined && userName !== null && password !== undefined && password!== null){
-
-    // }
-    return this.http.get<User>("http://localhost:3000/users").pipe(
-      map((user) => {
-        localStorage.setItem("user", JSON.stringify(user));
-        this.userSubject.next(user);
-        return user;
-      })
-    );
+  Login() {
+    return this.http.get<any>("http://localhost:3000/users").pipe();
   }
 }
